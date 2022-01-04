e5dfc91a8fdfd62ad8713b6ac749db2b38164a42
Nithyanandhan Velliyangiri <46887333+NickolusAlex@users.noreply.github.com>
2022-01-04 17:53:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ ReactDOM.render(
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="biddingapp/" element={<Home />}></Route>
+        <Route path="biddingapp/home" element={<Home />}></Route>
+        <Route path="biddingapp/about" element={<About />}></Route>
       </Routes>
       <Footer />
     </Router>
